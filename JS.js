a1225be7c331fff77232f0f907dd104c35e41269
Rushitha Reddy <rushithareddyponnella0202@gmail.com>
2024-11-14 22:42:13 +0530

    JS.js

--- JS.js
+++ JS.js
@@ -1,8 +1,8 @@
 document.querySelector('.view').addEventListener('click', () => {
-    window.open('https://drive.google.com/file/d/1FQZT8wzoyIpvuH27j3AKn-zIGodqcyor/view', '_blank');
+    window.open('https://drive.google.com/file/d/1ipw3MLJ0_g0TgPjwNzpV_ZJ9DWafVtGn/view', '_blank');
 });
 document.querySelector('.down').addEventListener('click', () => {
-    window.location.href = 'https://drive.google.com/uc?export=download&id=1FQZT8wzoyIpvuH27j3AKn-zIGodqcyor';
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1ipw3MLJ0_g0TgPjwNzpV_ZJ9DWafVtGn';
 });
 const iframe = document.getElementById('frame');
 iframe.onload = function() {
