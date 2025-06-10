c91c4aa41b92a319bc1df77a7b56d2e4bd6550cd
Rushitha Reddy <rushithareddyponnella0202@gmail.com>
2025-06-10 11:02:13 +0530

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,8 +1,8 @@
 document.querySelector('.view').addEventListener('click', () => {
-    window.open('https://drive.google.com/file/d/1ipw3MLJ0_g0TgPjwNzpV_ZJ9DWafVtGn/view', '_blank');
+    window.open('https://drive.google.com/file/d/1eapmmS-10P9id7XK_ArlRxoDqDD5yI0V/view', '_blank');
 });
 document.querySelector('.down').addEventListener('click', () => {
-    window.location.href = 'https://drive.google.com/uc?export=download&id=1ipw3MLJ0_g0TgPjwNzpV_ZJ9DWafVtGn';
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1eapmmS-10P9id7XK_ArlRxoDqDD5yI0V';
 });
 const iframe = document.getElementById('frame');
 iframe.onload = function() {
